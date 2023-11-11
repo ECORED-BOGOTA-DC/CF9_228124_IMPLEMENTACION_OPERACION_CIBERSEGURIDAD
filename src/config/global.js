3069006b1af6c343699024a599cf8238fb74a92b
Jhon Edinson Castañeda Oviedo <48686871+JhonEdinson-Shongo@users.noreply.github.com>
2023-11-10 20:58:39 -0500

    avances v1

--- src/config/global.js
+++ src/config/global.js
@@ -1,7 +1,8 @@
 export default {
   global: {
-    componenteFormativo: 'NOMBRE DEL COMPONENTE FORMATIVO',
-    descripcionCurso: 'BREVE DESCRIPCIÓN',
+    componenteFormativo: 'Planificación de la evaluación de la seguridad digital',
+    descripcionCurso:
+      'Mediante este componente, el aprendiz se capacitará en la identificación de aspectos clave del proceso de evaluación de estrategias de seguridad en organizaciones, fase orientada por el estándar ISO/IEC 27001:2013. Se afianzará en verificar la efectividad de controles de seguridad implementados y, a partir de esta evaluación, establecer propuestas de mejoramiento de la seguridad de activos de información.',
     imagenBannerPrincipal: require('@/assets/curso/portada/banner-princiapal.svg'),
     fondoBannerPrincipal: require('@/assets/curso/portada/fondo-banner-principal.png'),
     imagenesDecorativasBanner: [
@@ -31,27 +32,68 @@ export default {
       {
         nombreRuta: 'tema1',
         numero: '1',
-        titulo: 'Titulo de primer nivel',
+        titulo: 'Fases Sistema de gestión de seguridad de la información',
         desarrolloContenidos: true,
-        subMenu: [
-          {
-            numero: '1.1',
-            titulo: 'Titulo de segundo nivel',
-            hash: 't_1_1',
-          },
-        ],
       },
 
       {
         nombreRuta: 'tema2',
         numero: '2',
-        titulo: 'Titulo de primer nivel',
+        titulo: 'Técnicas de auditoría',
         desarrolloContenidos: true,
       },
       {
         nombreRuta: 'tema3',
         numero: '3',
-        titulo: 'Titulo de primer nivel',
+        titulo: 'Tipos de auditoría',
+        desarrolloContenidos: true,
+      },
+      {
+        nombreRuta: 'tema4',
+        numero: '4',
+        titulo: 'Definiciones y elementos fundamentales de la auditoría',
+        desarrolloContenidos: true,
+      },
+      {
+        nombreRuta: 'tema5',
+        numero: '5',
+        titulo: 'Consideraciones importantes para una auditoría',
+        desarrolloContenidos: true,
+      },
+      {
+        nombreRuta: 'tema6',
+        numero: '6',
+        titulo: 'Principios de auditoría',
+        desarrolloContenidos: true,
+      },
+      {
+        nombreRuta: 'tema7',
+        numero: '7',
+        titulo: 'Fases de la auditoría',
+        desarrolloContenidos: true,
+      },
+      {
+        nombreRuta: 'tema8',
+        numero: '8',
+        titulo: 'Clasificación de auditoría informáticas',
+        desarrolloContenidos: true,
+      },
+      {
+        nombreRuta: 'tema9',
+        numero: '9',
+        titulo: 'Perfil del auditor',
+        desarrolloContenidos: true,
+      },
+      {
+        nombreRuta: 'tema10',
+        numero: '10',
+        titulo: 'Metodología para la auditoría en sistemas',
+        desarrolloContenidos: true,
+      },
+      {
+        nombreRuta: 'tema11',
+        numero: '11',
+        titulo: 'Aplicación de mediciones de seguridad',
         desarrolloContenidos: true,
       },
     ],
@@ -86,7 +128,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/prueba.pdf',
+        download: 'downloads/228124_CF09_DU.pdf',
       },
       {
         icono: 'fas fa-download',
@@ -102,32 +144,62 @@ export default {
   },
   complementario: [
     {
-      tema: '',
-      referencia: '',
-      tipo: '',
-      link: 'https://www.google.com/',
-    },
-    {
-      tema: '',
-      referencia: '',
-      tipo: '',
-      descarga: '/downloads/prueba.pdf',
+      tema: 'Principios de auditoría',
+      referencia: 'Organización Internacional de Normalización (2013). <em>Seguridad de la información, ciberseguridad y protección de la privacidad.</em> (ISO 27001).',
+      tipo: 'Norma técnica',
+      link: 'https://www.iso.org/standard/54534.html',
     },
   ],
   glosario: [
     {
-      termino: 'Término',
-      significado: 'Texto de la definición',
+      termino: 'Atributo',
+      significado: 'cualquier propiedad o característica que permite distinguir un objeto de otro.',
     },
     {
-      termino: '<em>Término con extranjerismo</em>',
-      significado: 'Definición',
+      termino: 'Escala',
+      significado: 'rango de valores organizados con los cuales se evalúa un atributo.',
+    },
+    {
+      termino: 'Evidencia',
+      significado: 'información suficiente que respalda alguna acción.',
+    },
+    {
+      termino: 'Indicador',
+      significado: 'son unidades que permiten medir el desempeño o desarrollo de alguna acción o de algún control.',
+    },
+    {
+      termino: 'Métrica',
+      significado: 'conjunto de criterios y condiciones necesarios para medir un control o una acción.',
+    },
+    {
+      termino: 'SGSI',
+      significado: ' Sistema de Gestión de la Seguridad de la Información.',
     },
   ],
   referencias: [
     {
-      referencia: '',
-      link: '',
+      referencia: 'Instituto Nacional de Ciberseguridad (2015). <em>¿Sabes cómo se mide la seguridad de la información en tu empresa?</em> INCIBE. ',
+      link: 'https://www.incibe.es/protege-tu-empresa/blog/mide-seguridad-informacion',
+    },
+    {
+      referencia: 'ISO (2020). <em>Evaluación del desempeño en ISO 27001.</em> (ISO 27001).',
+      link: 'https://normaiso27001.es/evaluacion-del-desempeno-en-iso-27001/',
+    },
+    {
+      referencia: 'ISO (2020). <em>Fase 8 auditoría interna según ISO 27001.</em> (ISO 27001).',
+      link: 'https://normaiso27001.es/fase-8-auditoría-interna-segun-iso-27001/',
+    },
+    {
+      referencia: 'ISO (2018). <em>Directrices para la auditoría de los sistemas de gestión.</em> (ISO 19011)',
+      link: 'https://e-collection-icontec-org.bdigital.sena.edu.co/normavw.aspx?ID=75245',
+    },
+    {
+      referencia: 'Ministerio de Tecnologías de la Información y Comunicaciones (2016). <em>Guía de Auditoría.</em> MINTIC.',
+      link: 'https://www.mintic.gov.co/gestionti/615/articles-5482_G15_Auditoría.pdf',
+    },
+    {
+      referencia: 'Organización Internacional de Normalización (ISO, 2013). <em>Tecnología de la información - Técnicas de seguridad - Sistemas de gestión de seguridad de la información - Requisitos.</em> (ISO 27001).',
+      link: 'https://www.iso.org/standard/54534.html',
     },
   ],
   creditos: [
@@ -135,15 +207,15 @@ export default {
       titulo: 'ECOSISTEMA DE RECURSOS EDUCATIVOS DIGITALES',
       autores: [
         {
-          nombre: 'Claudia Patricia Aristizabal',
-          cargo: 'Responsable del Ecosistema',
+          nombre: 'Claudia Patricia Aristizábal Gutiérrez',
+          cargo: 'Responsable del equipo',
           centro: 'Dirección General',
         },
         {
-          nombre: 'Rafael Neftalí Lizcano Reyes',
-          cargo: 'Responsable de Línea de Producción',
+          nombre: 'Liliana Victoria Morales Gualdrón',
+          cargo: 'Responsable de línea de producción',
           centro:
-            'Centro Industrial del Diseño y la Manufactura - Regional Santander',
+            'Regional Distrito Capital - Centro de Gestión De Mercados, Logística y Tecnologías de la Información',
         },
       ],
     },
@@ -151,10 +223,46 @@ export default {
       titulo: 'CONTENIDO INSTRUCCIONAL',
       autores: [
         {
-          nombre: 'Nombre responsable',
-          cargo: 'Nombre del rol',
+          nombre: 'Eddy Cárdenas',
+          cargo: 'Experto temático',
           centro:
-            'Centro Industrial del Diseño y la Manufactura - Regional Santander',
+            'Regional Quindío - Centro de comercio, industria y turismo',
+        },
+        {
+          nombre: 'Zuleidy María Ruiz Torres',
+          cargo: 'Experto temático',
+          centro:
+            'Regional Tolima - Centro Comercio y Servicios',
+        },
+        {
+          nombre: 'Gloria Alexandra Orejarena Barrios',
+          cargo: 'Diseño instruccional',
+          centro:
+            'Regional Distrito Capital - Centro de Gestión Industrial',
+        },
+        {
+          nombre: 'Rafael Neftalí Lizcano Reyes',
+          cargo: 'Responsable equipo de desarrollo curricular',
+          centro:
+            'Regional Santander - Centro Industrial del Diseño y la Manufactura',
+        },
+        {
+          nombre: 'Jhon Jairo Rodríguez Pérez',
+          cargo: 'Corrección de estilo',
+          centro:
+            'Regional Distrito Capital - Centro de Diseño y Metrología',
+        },
+        {
+          nombre: 'Alix Cecilia Chinchilla Rueda',
+          cargo: 'Asesoría metodológica y pedagógica',
+          centro:
+            'Regional Distrito Capital - Centro de Gestión de Mercados, Logística y Tecnologías de la Información',
+        },
+        {
+          nombre: 'Nelly Parra Guarín',
+          cargo: 'Adecuación instruccional',
+          centro:
+            'Regional Distrito Capital - Centro de Gestión de Mercados, Logística y Tecnologías de la Información',
         },
       ],
     },
@@ -162,28 +270,28 @@ export default {
       titulo: 'DISEÑO Y DESARROLLO DE RECURSOS EDUCATIVOS DIGITALES',
       autores: [
         {
-          nombre: 'Nombre responsable',
-          cargo: 'Diseñador de Contenidos Digitales',
+          nombre: 'Adriana Marcela Suarez Eljure',
+          cargo: 'Diseño web',
           centro:
-            'Centro Industrial del Diseño y la Manufactura - Regional Santander',
+            'Regional Distrito Capital - Centro de Gestión de Mercados, Logística y Tecnologías de la Información',
         },
         {
-          nombre: 'Nombre responsable',
-          cargo: 'Desarrollador Fullstack',
+          nombre: 'Jhon Edinson Castañeda Oviedo',
+          cargo: 'Desarrollo Fullstack',
           centro:
-            'Centro Industrial del Diseño y la Manufactura - Regional Santander',
+            'Regional Distrito Capital - Centro de Gestión de Mercados, Logística y Tecnologías de la Información',
+        },
+        {
+          nombre: 'Adriana Marcela Suarez Eljure',
+          cargo: 'Ilustración',
+          centro:
+            'Regional Distrito Capital - Centro de Gestión de Mercados, Logística y Tecnologías de la Información',
         },
         {
           nombre: 'Nombre',
-          cargo: 'Animador y Producción audiovisual',
+          cargo: 'Animación y producción audiovisual',
           centro:
-            'Centro Industrial del Diseño y la Manufactura - Regional Santander',
-        },
-        {
-          nombre: 'Nombre',
-          cargo: 'Actividad Didáctica',
-          centro:
-            'Centro Industrial del Diseño y la Manufactura - Regional Santander',
+            'Regional Distrito Capital - Centro de Gestión de Mercados, Logística y Tecnologías de la Información',
         },
       ],
     },
@@ -191,22 +299,22 @@ export default {
       titulo: 'VALIDACIÓN RECURSO EDUCATIVO DIGITAL',
       autores: [
         {
-          nombre: 'Zuleidy María Ruiz Torres',
-          cargo: 'Validador de Recursos Educativos Digitales',
+          nombre: 'Carolina Coca Salazar',
+          cargo: 'Evaluación de contenidos inclusivos y accesibles',
           centro:
-            'Centro Industrial del Diseño y la Manufactura - Regional Santander',
+            'Regional Distrito Capital - Centro de Gestión De Mercados, Logística y Tecnologías de la Información',
         },
         {
-          nombre: 'Luis Gabriel Urueta Alvarez',
-          cargo: 'Validador de Recursos Educativos Digitales',
+          nombre: 'Lina Marcela Pérez Manchego',
+          cargo: 'Validación de recursos educativos digitales',
           centro:
-            'Centro Industrial del Diseño y la Manufactura - Regional Santander',
+            'Regional Distrito Capital - Centro de Gestión De Mercados, Logística y Tecnologías de la Información',
         },
         {
-          nombre: 'Daniel Ricardo Mutis Gómez',
-          cargo: 'Evaluador para contenidos inclusivos y accesibles',
+          nombre: 'Leyson Fabián Castaño Pérez',
+          cargo: 'Validación de recursos educativos digitales y vinculación LMS',
           centro:
-            'Centro Industrial del Diseño y la Manufactura - Regional Santander',
+            'Centro de Gestión De Mercados, Logística y Tecnologías de la Información - Regional Distrito Capital',
         },
       ],
     },
