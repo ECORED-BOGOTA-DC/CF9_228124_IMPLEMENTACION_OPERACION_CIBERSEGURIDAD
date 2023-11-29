73a3136ffb5e40dae24a2b1843ac3c5cd371296b
Jhon Edinson Castañeda Oviedo <48686871+JhonEdinson-Shongo@users.noreply.github.com>
2023-11-29 17:11:31 -0500

    Ajustes finales diseño

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -13,9 +13,17 @@
         img(src="@/assets/curso/temas/img-1.svg" style="max-width: 85px" alt="Imagen decorativa de un documento" data-aos="fade-right")
         p.ps-md-4.mb-0(data-aos="fade-left") Le invitamos a adentrarse en el estudio de este componente, reconociendo el valor fundamental que tiene para su formación como tecnólogo/a en implementación y operación de la ciberseguridad.
       
-      figure(data-aos="fade-left")
-        .video
-          iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+      .d-flex.flex-wrap
+        LineaTiempoD.color-acento-contenido.col-lg-8.pe-lg-4
+          p(numero="1" titulo="Contexto Norma ISO/IEC 27001:2013") Dentro del ciclo que enmarca la norma ISO/IEC 27001:2013, se establece una fase que permite evaluar el rendimiento del sistema de seguridad de la información y, en especial, los controles que fueron propuestos para mejorar la seguridad de los activos de información de la organización.
+          p(numero="2" titulo="Evaluación Norma ISO/IEC 27001:2013") Esta fase implica establecer una métrica, bajo la cual se identifiquen los rangos de valores aceptables y no aceptables, de cada control. 
+            br
+            br
+            |A partir de esta evaluación se podrán establecer los mecanismos de mejora de los mismos controles, o de cambio completo de un determinado control, en caso de ser necesario.
+          p(numero="3" titulo="Evaluación estrategia de seguridad ") “Lo que no se mide no se puede mejorar. Lo que no se mejora, se degrada siempre” William Thomson Kelvin
+            br
+            |Se considera que la evaluación de la estrategia de seguridad deba realizarse de manera periódica, convirtiéndose en punto de partida para la adopción de la mejora continua.
+        img.mb-auto.d-none.d-lg-flex.col-lg-4(src="@/assets/curso/temas/img-2.svg" style="max-width: 400px" alt="Imagen decorativa" data-aos="fade-left")
 </template>
 
 <script>
