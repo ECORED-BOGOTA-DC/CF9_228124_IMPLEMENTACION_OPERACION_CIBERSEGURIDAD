312fa118475355e21d290846633de97c4320b3a8
Jhon Edinson Castañeda Oviedo <48686871+JhonEdinson-Shongo@users.noreply.github.com>
2023-11-30 14:45:28 -0500

    Ajuste texto introducción

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -8,11 +8,11 @@
         span
           i.fas.fa-info
       h1 Introducción
-    .container-100.pt-4(style="background: linear-gradient(180deg, rgba(220,206,227,1) 50%, rgba(255,255,255,1) 50%);")
+    .container-100.pt-4(style="background: linear-gradient(180deg, rgba(220,206,227,1) 60%, rgba(255,255,255,1) 60%);")
       .d-flex.flex-wrap.flex-md-nowrap.align-items-center.mb-5
         img(src="@/assets/curso/temas/img-1.svg" style="max-width: 85px" alt="Imagen decorativa de un documento" data-aos="fade-right")
         p.ps-md-4.mb-0(data-aos="fade-left") Le invitamos a adentrarse en el estudio de este componente, reconociendo el valor fundamental que tiene para su formación como tecnólogo/a en implementación y operación de la ciberseguridad.
-      
+      p.mb-5 Se describen los desafíos y riesgos a los que las empresas se enfrentan en el entorno digital actual, así como los mejores procedimientos y regulaciones para garantizar la seguridad de la información.
       .d-flex.flex-wrap
         LineaTiempoD.color-acento-contenido.col-lg-8.pe-lg-4
           p(numero="1" titulo="Contexto Norma ISO/IEC 27001:2013") Dentro del ciclo que enmarca la norma ISO/IEC 27001:2013, se establece una fase que permite evaluar el rendimiento del sistema de seguridad de la información y, en especial, los controles que fueron propuestos para mejorar la seguridad de los activos de información de la organización.
